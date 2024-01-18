eab70ff4274709b8271522a268301799a27cae83
Hirakjyoti Talukdar <u20cse1041@cit.ac.in>
2024-01-18 13:37:35 +0530

    added the initial changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Tic-Tac-Toe-game
+
+## On Build 🪛
---
+++ lib/game.dart
@@ -0,0 +1,235 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'constants/colors.dart';
+import 'package:tic_tac_toe/main.dart';
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key});
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  bool oTurn = true;
+  List<String> displayXO = ['', '', '', '', '', '', '', '', ''];
+
+  int oScore = 0;
+  int xScore = 0;
+  int filledBoxed = 0;
+  String resultDec = '';
+  bool winnerFound = false;
+
+  static var customFontWhite = GoogleFonts.salsa(
+      textStyle: TextStyle(
+    color: Colors.white,
+    letterSpacing: 3,
+    fontSize: 28,
+  ));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: MainColor.primaryColor,
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Expanded(
+                flex: 1,
+                child: Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            'Player 0',
+                            style: customFontWhite,
+                          ),
+                          Text(
+                            oScore.toString(),
+                            style: customFontWhite,
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            'Player X',
+                            style: customFontWhite,
+                          ),
+                          Text(
+                            xScore.toString(),
+                            style: customFontWhite,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )),
+            Expanded(
+              flex: 3,
+              child: GridView.builder(
+                  itemCount: 9,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                  ),
+                  itemBuilder: (BuildContext context, int index) {
+                    return GestureDetector(
+                      onTap: () {
+                        _tapped(index);
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          border: Border.all(
+                            color: MainColor.primaryColor,
+                            width: 5,
+                          ),
+                          color: MainColor.secondaryColor,
+                        ),
+                        child: Center(
+                          child: Text(
+                            displayXO[index],
+                            style: GoogleFonts.salsa(
+                                textStyle: TextStyle(
+                              fontSize: 65,
+                              color: MainColor.primaryColor,
+                            )),
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+            Expanded(
+              flex: 2,
+              child: Text(
+                resultDec,
+                style: customFontWhite,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _tapped(int index) {
+    setState(() {
+      if (oTurn && displayXO[index] == '') {
+        displayXO[index] = '0';
+        filledBoxed++;
+      } else if (!oTurn && displayXO[index] == '') {
+        displayXO[index] = 'X';
+        filledBoxed++;
+      }
+
+      oTurn = !oTurn;
+      _checkWinner();
+    });
+  }
+
+  void _checkWinner() {
+    //check the staring row
+    if (displayXO[0] == displayXO[1] &&
+        displayXO[0] == displayXO[2] &&
+        displayXO[0] != '') {
+      setState(() {
+        resultDec = 'Player' + displayXO[0] + 'Wins!';
+        _updateScore(displayXO[0]);
+      });
+    }
+
+    //second row
+    if (displayXO[3] == displayXO[4] &&
+        displayXO[3] == displayXO[5] &&
+        displayXO[3] != '') {
+      setState(() {
+        resultDec = 'Player' + displayXO[3] + 'Wins!';
+        _updateScore(displayXO[3]);
+      });
+    }
+
+    //third row
+    if (displayXO[6] == displayXO[7] &&
+        displayXO[6] == displayXO[8] &&
+        displayXO[6] != '') {
+      setState(() {
+        resultDec = 'Player' + displayXO[6] + 'Wins!';
+        _updateScore(displayXO[6]);
+      });
+    }
+
+    //for the first column
+    if (displayXO[0] == displayXO[3] &&
+        displayXO[0] == displayXO[6] &&
+        displayXO[0] != '') {
+      setState(() {
+        resultDec = 'Player' + displayXO[0] + 'Wins!';
+        _updateScore(displayXO[0]);
+      });
+    }
+
+    //for the second column
+    if (displayXO[1] == displayXO[4] &&
+        displayXO[1] == displayXO[7] &&
+        displayXO[1] != '') {
+      setState(() {
+        resultDec = 'Player' + displayXO[1] + 'Wins!';
+        _updateScore(displayXO[1]);
+      });
+    }
+
+    //for the third column
+    if (displayXO[2] == displayXO[5] &&
+        displayXO[2] == displayXO[8] &&
+        displayXO[2] != '') {
+      setState(() {
+        resultDec = 'Player' + displayXO[2] + 'Wins!';
+        _updateScore(displayXO[2]);
+      });
+    }
+
+    // check right diagonal
+    if (displayXO[0] == displayXO[4] &&
+        displayXO[0] == displayXO[8] &&
+        displayXO[0] != '') {
+      setState(() {
+        resultDec = 'Player' + displayXO[0] + 'Wins!';
+        _updateScore(displayXO[0]);
+      });
+    }
+
+    //left diagonal
+    if (displayXO[6] == displayXO[4] &&
+        displayXO[6] == displayXO[2] &&
+        displayXO[6] != '') {
+      setState(() {
+        resultDec = 'Player' + displayXO[6] + 'Wins!';
+        _updateScore(displayXO[6]);
+      });
+    }
+
+    if ( !winnerFound && filledBoxed == 9) {
+      setState(() {
+        resultDec = 'Nobody Wins XD !';
+      });
+    }
+  }
+
+  void _updateScore(String winner) {
+    if (winner == 'O') {
+      oScore++;
+    } else if (winner == 'X') {
+      xScore++;
+    }
+    winnerFound = true;
+  }
+}
